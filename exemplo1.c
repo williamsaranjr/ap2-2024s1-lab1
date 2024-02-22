ef260b7d7f1c1100f03c8585e108079ad189b2ef
William Saran <134084734+williamsaranjr@users.noreply.github.com>
2024-02-22 00:58:58 +0000

    adicionado fundação da fci

--- exemplo1.c
+++ exemplo1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Mackenzie - FCI\n");
+  printf("Mackenzie - FCI - 1970\n");
   printf("Algoritmos e Programação II\n");
   printf("Programação em C\n");
   return 0;
